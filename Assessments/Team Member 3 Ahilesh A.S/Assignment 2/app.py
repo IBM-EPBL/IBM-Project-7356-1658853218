8860c213272559b6a1c3819ab1eeaf6602cbafac
ASAhilesh <113665782+ASAhilesh@users.noreply.github.com>
2022-10-11 23:52:08 -0700

    Add files via upload

---
+++ Assessments/Team Member 3 Ahilesh A.S/Assignment 2/app.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+       return render_template('home.html')
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+
+
+@app.route('/signin')
+def sign_in():
+      return render_template('signin.html')
+
+@app.route('/signup')
+def sign_up():
+    return render_template('signup.html')
+
+
+
+
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0" , port=5000 ,debug=True)
+
+# docker build -t ahilesh.
